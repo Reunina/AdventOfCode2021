de0796e7403d44d959953b07c938a536ea5a91df
sara <sara.tahabik@gmail.com>
2021-12-02 08:35:33 +0100

    Improve mix task 'create'

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule AdventOfCode2021.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :inets]
     ]
   end
 
