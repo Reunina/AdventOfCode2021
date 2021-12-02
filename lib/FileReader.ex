64f84e342ec5d1267018db677e48c41a7767c211
sara <sara.tahabik@gmail.com>
2021-12-02 07:59:52 +0100

    Day 02: Dive!

--- lib/FileReader.ex
+++ lib/FileReader.ex
@@ -1,4 +1,10 @@
 defmodule FileReader do
+  def read_file(file_name, :as_list_string_int) do
+    File.stream!(file_name)
+    |> Enum.map(fn line -> String.split(line) end)
+    |> Enum.map(fn [a, b] -> [a, b |> Integer.parse() |> elem(0)] end)
+  end
+
   def read_file(file_name, :as_int) do
     File.stream!(file_name)
     |> Enum.map(fn line -> String.trim(line) |> String.to_integer() end)
---
+++ test/day02_test.exs
@@ -0,0 +1,34 @@
+defmodule Day02Test do
+  use ExUnit.Case
+  alias FileReader
+  doctest Day02
+
+  alias Day02, as: Day
+
+  @test_input "inputs/tests/day_02.txt"
+  @real_input "inputs/day_02.txt"
+
+  test "day 02 part 1" do
+    assert 150 =
+             @test_input
+             |> FileReader.read_file(:as_list_string_int)
+             |> Day.part_01()
+
+    assert 2_070_300 =
+             @real_input
+             |> FileReader.read_file(:as_list_string_int)
+             |> Day.part_01()
+  end
+
+  test "day 02 part 2" do
+    assert 900 =
+             @test_input
+             |> FileReader.read_file(:as_list_string_int)
+             |> Day.part_02()
+
+    assert 2_078_985_210 =
+             @real_input
+             |> FileReader.read_file(:as_list_string_int)
+             |> Day.part_02()
+  end
+end
