c17e47fbb373421bff4bc7df44421f04c0e8e984
sara <sara.tahabik@gmail.com>
2021-12-04 06:26:35 +0100

    Day 3: Binary Diagnostic

--- lib/utilities/FileReader.ex
+++ lib/utilities/FileReader.ex
@@ -13,7 +13,7 @@ defmodule FileReader do
       # a 12
       # z 9
       # u -5
-      FileReader.read_file(input_file, :as_list_string_int)
+      FileReader.read_file(input_file, :as_string_and_int)
       [ ["a", 12], ["z",  9], ["u", -5] ]
 
       #=> in input_file :
@@ -26,15 +26,28 @@ defmodule FileReader do
   """
   def read_file(file_name, output_format)
 
+  def read_file(file_name, :as_ints_list) do
+    file_name
+    |> File.stream!()
+    |> Enum.map(fn line ->
+      line
+      |> String.replace("\n", "")
+      |> String.split("", trim: true)
+      |> Enum.map(fn x -> String.to_integer(x) end)
+    end)
+
+  end
+
   def read_file(file_name, :as_string_and_int) do
     File.stream!(file_name)
     |> Enum.map(fn line -> String.split(line) end)
-    |> Enum.map(fn [a, b] -> [a, b |> Integer.parse() |> elem(0)] end)
+    |> Enum.map(fn [a, b] -> [a, b |> String.trim() |> String.to_integer()] end)
   end
 
   def read_file(file_name, :as_int) do
-    File.stream!(file_name)
-    |> Enum.map(fn line -> String.trim(line) |> String.to_integer() end)
+    file_name
+    |> File.stream!()
+    |> Enum.map(fn line -> line |> String.trim() |> String.to_integer() end)
   end
 
   @doc """
@@ -43,7 +56,8 @@ defmodule FileReader do
   Returns list.
   """
   def read_file(file_name) do
-    File.stream!(file_name)
+    file_name
+    |> File.stream!()
     |> Enum.map(&String.trim/1)
   end
 end
---
+++ test/day03_test.exs
@@ -0,0 +1,34 @@
+defmodule Day03Test do
+  use ExUnit.Case
+  alias FileReader
+  doctest Day03
+
+  alias Day03, as: Day
+
+  @test_input "inputs/tests/day_03.txt"
+  @real_input "inputs/day_03.txt"
+
+  test "day 3 part 1" do
+    assert 198 =
+             @test_input
+             |> FileReader.read_file(:as_ints_list)
+             |> Day.part_01()
+
+    assert 3_309_596 =
+             @real_input
+             |> FileReader.read_file(:as_ints_list)
+             |> Day.part_01()
+  end
+
+  test "day 3 part 2" do
+    assert 230 =
+             @test_input
+             |> FileReader.read_file(:as_ints_list)
+             |> Day.part_02()
+
+    assert 2_981_085 =
+             @real_input
+             |> FileReader.read_file(:as_ints_list)
+             |> Day.part_02()
+  end
+end
