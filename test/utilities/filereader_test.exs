0ef41c69773093a2b68c53c91c9d498f0d0fa657
sara <sara.tahabik@gmail.com>
2021-12-05 11:01:22 +0100

    Filereader test read input as ints

--- test/utilities/filereader_test.exs
+++ test/utilities/filereader_test.exs
@@ -11,7 +11,7 @@ defmodule FileReaderTest do
     end
 
     test "read simply input as trimmed strings" do
-      assert  ["first line", "1", "5.4", "é"] =
+      assert ["first line", "1", "5.4", "é"] =
                """
                first line
                1
@@ -22,11 +22,34 @@ defmodule FileReaderTest do
                |> FileReader.read_file()
     end
 
+    test "read simply input as integers" do
+      assert_raise ArgumentError,
+                   "errors were found at the given arguments:\n\n  * 1st argument: not a textual representation of an integer\n",
+                   fn ->
+                     """
+                     first line
+                     1
+                     5.4
+
+                     é
+                     """
+                     |> write_in_test_file()
+                     |> FileReader.read_file(:as_int)
+                   end
+
+      assert [1, 2, 3] =
+               """
+               1
+               2
+               3
+               """
+               |> write_in_test_file()
+               |> FileReader.read_file(:as_int)
+    end
+
     defp write_in_test_file(content) do
-                                      @test_file |> File.write(content)
-                                      @test_file
-                                      end
-
+      @test_file |> File.write(content)
+      @test_file
+    end
   end
-
 end
